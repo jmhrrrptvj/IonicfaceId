dc72446441bbfd1fc73d1bfa1b2e6c173f3bc64d
Kannan <kannank@pixmonks.com>
2019-09-24 11:08:37 +0530

    Initial Commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+declare var CID: any;
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
@@ -9,6 +11,20 @@ export class HomePage {
 
   constructor(public navCtrl: NavController) {
 
+  }
+
+  checkAuth(reason: string) {
+  	CID.checkAuth(reason, function(res) {
+      if (res == 'success') {
+        alert('success');
+      } else {
+        alert(res);
+        // console.log(res);
+      }
+  	}, function(err) {
+        alert(err); 
+  		//console.log(err);
+  	});
   }
 
 }
